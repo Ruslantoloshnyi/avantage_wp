003ce0bcaec88ee0e904c3fe2ab59a4f1ca382ae
Ruslan <ru@skylinehealthgroup.com>
2023-08-03 16:08:47 +0300

    fix booking template and css

--- template-parts/booking_page_templates/booking_calculation.php
+++ template-parts/booking_page_templates/booking_calculation.php
@@ -78,18 +78,38 @@ $img6 = wp_get_attachment_image($image6, 'full', false, $attr);
 
                 <div class="col-md-6 col-12">
 
-                <div class="row">
+                    <div class="row">
+                        <div class="col-md-6 col-12">
+                            <div class="container-calendar">
+                                <div class="calendar-head">
+                                    <p>Дата заїзду:</p>
+                                </div>
 
-                    <div class="col-md-6 col-12">
-                        <div class="container-calendar">
+                                <form>
+                                    <div class="row form-group">
+                                        <div class="input-group date" id="datepicker">
+                                            <input id="check-in" type="text" class="form-control">
+                                            <span class="input-group-append">
+                                                <span class="input-group-text bg-white">
+                                                    <i class="fa fa-calendar"></i>
+                                                </span>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-12">
+
                             <div class="calendar-head">
-                                <p>Дата заїзду:</p>
+                                <p>Дата виїзду:</p>
                             </div>
 
                             <form>
                                 <div class="row form-group">
-                                    <div class="input-group date" id="datepicker">
-                                        <input id="check-in" type="text" class="form-control">
+                                    <div class="input-group date" id="datepicker-2">
+                                        <input id="check-out" data-key="1" type="text" class="form-control">
                                         <span class="input-group-append">
                                             <span class="input-group-text bg-white">
                                                 <i class="fa fa-calendar"></i>
@@ -99,39 +119,18 @@ $img6 = wp_get_attachment_image($image6, 'full', false, $attr);
                                 </div>
                             </form>
                         </div>
+
                     </div>
 
-                    <div class="col-md-6 col-12">
-
-                        <div class="calendar-head">
-                            <p>Дата виїзду:</p>
-                        </div>
-
-                        <form>
-                            <div class="row form-group">
-                                <div class="input-group date" id="datepicker-2">
-                                    <input id="check-out" data-key="1" type="text" class="form-control">
-                                    <span class="input-group-append">
-                                        <span class="input-group-text bg-white">
-                                            <i class="fa fa-calendar"></i>
-                                        </span>
-                                    </span>
-                                </div>
-                            </div>
-                        </form>
+                    <div id="" class="booking-text">
+                        <p><span class="booking-span-title">Всього:</span> <span id="price" class=""></span><span id="nights"></span><br>
+                            <span class="booking-text-content">
+                                Бронювання здійснюється за телефоном або електронною поштою.
+                            </span>
+                        </p>
                     </div>
 
                 </div>
-
-                <div id="" class="col-12 booking-text">
-                    <p><span class="booking-span-title">Всього:</span> <span id="price" class=""></span><span id="nights"></span><br>
-                        <span class="booking-text-content">
-                            Бронювання здійснюється за телефоном або електронною поштою.
-                        </span>
-                    </p>
-                </div>
-
-            </div>
             </div>
         </div>
     </div>
--- template-parts/booking_page_templates/booking_table.php
+++ template-parts/booking_page_templates/booking_table.php
@@ -1,21 +1,17 @@
 <?php
-$tel_1 = get_field('tel_1' , 'option');
+$tel_1 = get_field('tel_1', 'option');
 $tel_2 = get_field('tel_2', 'option');
 $arrPrice = [];
 $repeater_table_field = get_field('table', 'option');
 ?>
 
 <?php if ($repeater_table_field) : ?>
-
     <!-- Booking table section
     ================================================== -->
-    <section class="booking-table">
-
-        <div class="col-12">
-
-            <div class="row">
-
-                <div class="container">
+    <div class="container">
+        <section class="booking-table">
+            <div class="col-12">
+                <div class="row">
 
                     <table>
                         <tr>
@@ -27,10 +23,8 @@ $repeater_table_field = get_field('table', 'option');
                             <th>Люкс для чотирьох</th>
                             <th>Двокімнатний люкс<br>для чотирьох</th>
                         </tr>
-
                         <?php while (have_rows('table', 'option')) : the_row();
                             $head = get_sub_field('table_head');
-
                         ?>
                             <tr>
                                 <td class="booking-table-one"><?php echo $head; ?></td>
@@ -38,56 +32,41 @@ $repeater_table_field = get_field('table', 'option');
                                 <?php while (have_rows('table_subhead', 'option')) : the_row();
                                     $price = get_sub_field('table_price', 'option');
                                 ?>
-                                    <?php array_push($arrPrice, $price); ?>
-
                                     <td><?php echo $price; ?></td>
                                 <?php endwhile; ?>
                             </tr>
                         <?php endwhile; ?>
                     </table>
+
                 </div>
             </div>
-        </div>
-    </section> <!-- Booking table section End -->
-
+        </section>
+    </div>
 <?php endif; ?>
-
-<script>
-    <?php echo "var arrPrice=" . json_encode($arrPrice, JSON_FORCE_OBJECT); ?>
-</script>
 
 <!-- Booking map section
     ================================================== -->
 <section class="map-section">
-
     <div class="booking-head">
         <h1>Адреса</h1>
     </div>
-
     <div class="container">
         <div class="row">
             <div class="col-md-6 col-12">
                 <div class="booking-map-text">
                     <p>Наша адреса: вулиця Шкільна, 59Б, Залізний Порт, Херсонська область, 75653</p>
                 </div>
-
                 <div class="booking-map-text">
                     <p>Телефони: <?php echo $tel_1; ?>;<br>
                         <span><?php echo $tel_2; ?></span>
                     </p>
                 </div>
             </div>
-
             <div class="col-md-6 col-12">
-
                 <div class="map-responsive">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5530.925745666488!2d32.28159932597306!3d46.12160893929788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfe79e8540310af6f!2z0JDQstCw0L3RgtCw0LY!5e0!3m2!1sru!2sde!4v1668270806167!5m2!1sru!2sde" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-
             </div>
-
         </div>
-
     </div>
-
 </section> <!-- Booking map section End -->
